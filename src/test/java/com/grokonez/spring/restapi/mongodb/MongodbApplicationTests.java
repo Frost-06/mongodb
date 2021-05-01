8f83f3d158f4763faba8310d54bf9d8eea3e9ae9
Fros-06 <andredulaguinita@gmail.com>
2021-05-02 01:04:10 +0800

    init

---
+++ src/test/java/com/grokonez/spring/restapi/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grokonez.spring.restapi.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
